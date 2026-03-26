986d45288cb648e3c48ce7d23425776299096773
Ainhoa <ainhoatomas@gmail.com>
2026-03-26 18:48:25 +0100

    Bektorizazio Probak

--- MovieReview/src/MovieReview/BektorizazioProbak.java
+++ MovieReview/src/MovieReview/BektorizazioProbak.java
@@ -1,111 +1,102 @@
 package MovieReview;
 
-import weka.core.converters.ConverterUtils.DataSource;
-import weka.core.stemmers.LovinsStemmer;
-import weka.core.stopwords.Rainbow;
-import weka.core.tokenizers.AlphabeticTokenizer;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.filters.supervised.attribute.AttributeSelection;
-import weka.attributeSelection.*;
-import weka.classifiers.bayes.NaiveBayes;
+
+import weka.attributeSelection.InfoGainAttributeEval;
+import weka.attributeSelection.GainRatioAttributeEval;
+import weka.attributeSelection.Ranker;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.functions.SMO;
 import weka.classifiers.Evaluation;
 
 public class BektorizazioProbak {
+
     public static void main(String[] args) throws Exception {
 
-        // =====================
-        // 1. Datuak kargatu
-        // =====================
-        DataSource trainSource = new DataSource("datuak/train.arff");
-        Instances train = trainSource.getDataSet();
-        train.setClassIndex(train.numAttributes() - 1);
+        // 📥 Cargar datasets
+        Instances train = new DataSource("datuak/train.arff").getDataSet();
+        Instances dev = new DataSource("datuak/dev.arff").getDataSet();
 
-        DataSource devSource = new DataSource("datuak/dev.arff");
-        Instances dev = devSource.getDataSet();
+        train.setClassIndex(train.numAttributes() - 1);
         dev.setClassIndex(dev.numAttributes() - 1);
 
-        // =====================
-        // 2. Aukerak
-        // =====================
-        boolean[] tfOptions = {false, true};
-        boolean[] idfOptions = {false, true};
+        // 🔁 Probar TODAS las combinaciones
+        String[] vectorTypes = {"BoW", "TF", "TF-IDF"};
+        String[] attrSelTypes = {"InfoGain", "GainRatio"};
 
-        ASEvaluation[] evaluators = {
-            new InfoGainAttributeEval(),
-            new GainRatioAttributeEval()
-        };
-
-        String[] evalNames = {
-            "InfoGain",
-            "GainRatio"
-        };
-
-        // =====================
-        // 3. Konbinazio guztiak
-        // =====================
-        for (boolean tf : tfOptions) {
-            for (boolean idf : idfOptions) {
-
-                if (!tf && idf) continue;
-
-                System.out.println("\n==============================");
-                System.out.println("Vectorization: TF=" + tf + " IDF=" + idf);
-
-                // =====================
-                // 4. Bektorizazioa
-                // =====================
-                StringToWordVector filter = new StringToWordVector();
-                filter.setTokenizer(new AlphabeticTokenizer());
-                filter.setStemmer(new LovinsStemmer());
-                filter.setStopwordsHandler(new Rainbow());
-
-                filter.setTFTransform(tf);
-                filter.setIDFTransform(idf);
-                filter.setLowerCaseTokens(true);
-                filter.setWordsToKeep(5000);
-
-                filter.setInputFormat(train);
-
-                Instances trainBek = Filter.useFilter(train, filter);
-                Instances devBek = Filter.useFilter(dev, filter);
-
-                // =====================
-                // 5. Atributuen selekzioa
-                // =====================
-                for (int i = 0; i < evaluators.length; i++) {
-
-                    AttributeSelection attr = new AttributeSelection();
-                    Ranker ranker = new Ranker();
-                    ranker.setNumToSelect(1000);
-
-                    attr.setEvaluator(evaluators[i]);
-                    attr.setSearch(ranker);
-                    attr.setInputFormat(trainBek);
-
-                    Instances trainSel = Filter.useFilter(trainBek, attr);
-                    Instances devSel = Filter.useFilter(devBek, attr);
-
-                    // =====================
-                    // 6. Modeloa + ebaluazioa
-                    // =====================
-                    NaiveBayes nb = new NaiveBayes();
-                    nb.buildClassifier(trainSel);
-
-                    Evaluation eval = new Evaluation(trainSel);
-                    eval.evaluateModel(nb, devSel);
-
-                    // =====================
-                    // 7. Emaitzak
-                    // =====================
-                    System.out.println("\n--- " + evalNames[i] + " ---");
-                    System.out.println("Attributes: " + trainSel.numAttributes());
-                    System.out.println("Accuracy: " + eval.pctCorrect());
-                    System.out.println(eval.toSummaryString());
-                }
-                
+        for (String v : vectorTypes) {
+            for (String a : attrSelTypes) {
+                testConfiguration(train, dev, v, a);
             }
         }
+    }
+
+    public static void testConfiguration(Instances train, Instances dev,
+                                         String vectorType, String attrSelType) throws Exception {
+
+        System.out.println("====================================");
+        System.out.println("Vectorization: " + vectorType + " | AttributeSelection: " + attrSelType);
+
+        // 🔹 1. StringToWordVector
+        StringToWordVector filter = new StringToWordVector();
+        filter.setWordsToKeep(5000);
+        filter.setLowerCaseTokens(true);
+
+        if (vectorType.equals("BoW")) {
+            filter.setOutputWordCounts(false);
+            filter.setTFTransform(false);
+            filter.setIDFTransform(false);
+        } else if (vectorType.equals("TF")) {
+            filter.setOutputWordCounts(true);
+            filter.setTFTransform(true);
+            filter.setIDFTransform(false);
+        } else if (vectorType.equals("TF-IDF")) {
+            filter.setOutputWordCounts(true);
+            filter.setTFTransform(true);
+            filter.setIDFTransform(true);
+        }
+
+        filter.setInputFormat(train);
+
+        Instances trainFiltered = Filter.useFilter(train, filter);
+        Instances devFiltered = Filter.useFilter(dev, filter);
+
+        // 🔹 2. Attribute Selection
+        AttributeSelection attrSel = new AttributeSelection();
+        Ranker ranker = new Ranker();
+        ranker.setNumToSelect(1000);
+
+        if (attrSelType.equals("InfoGain")) {
+            attrSel.setEvaluator(new InfoGainAttributeEval());
+        } else if (attrSelType.equals("GainRatio")) {
+            attrSel.setEvaluator(new GainRatioAttributeEval());
+        }
+
+        attrSel.setSearch(ranker);
+        attrSel.setInputFormat(trainFiltered);
+
+        trainFiltered = Filter.useFilter(trainFiltered, attrSel);
+        devFiltered = Filter.useFilter(devFiltered, attrSel);
+
+        // 🔹 3. Clasificador: SMO (SVM)
+        SMO cls = new SMO();
+        cls.buildClassifier(trainFiltered);
+
+        // 🔹 4. Evaluación en dev
+        Evaluation eval = new Evaluation(trainFiltered);
+        eval.evaluateModel(cls, devFiltered);
+
+        // 🔹 5. Resultados en %
+        double accuracy = (1 - eval.errorRate()) * 100;
+        double f1 = eval.fMeasure(1) * 100;
+
+        System.out.printf("Accuracy: %.2f%%\n", accuracy);
+        System.out.printf("F1 (pos): %.2f%%\n", f1);
+        System.out.println("Num attributes: " + trainFiltered.numAttributes());
     }
 }
